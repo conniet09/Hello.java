7df826ed499e3558c76bdccf1c2d1a54ec78a31a
conniet09 <59925548+conniet09@users.noreply.github.com>
2020-01-15 10:12:03 -0500

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,20 @@
+/***************************
+*Hello.java
+****************************
+*Connie Truong
+*January 15, 2020
+*CMSC255 and Section 003
+****************************/
+
+public class Hello {
+
+	public static void main(String[] args) {
+
+		System.out.println("Connie Truong");
+		System.out.println("Project 1");
+		System.out.println("Name in Code");
+		System.out.println("January 15 2020");
+		System.out.println("Course 255 and Section 003");
+		System.out.println();
+		}
+}
